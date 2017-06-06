40815bd137016afbd7af7b057257b97834e6637a
Alvaco <vas09284@uvg.edu.gt>
2017-06-06 09:53:36 -0600

    Update principal.py

--- principal.py
+++ principal.py
@@ -2,29 +2,26 @@
 """
 Created on Tue Jun  6 01:50:57 2017
 
-@author: Miguel
+@author: Alejandro Vásquez, Marlon Fuentes, Davis
 """
 
 from proyecto2 import *
 
 
-def menu():
-    resp = 0
-    while resp==0:
-        print ("Bienvenido!"+"\n"
+
+while True:
+    print ("Bienvenido!"+"\n"
                +"1. Realizar una reservacion nueva \n"
                +"2. Eliminar una reservacion creada \n"
                +"3. Crear un vuelo\n"
                +"4. Eliminar vuelo\n"
                +"5. Modificar hora de vuelo\n"
                +"6. Modificar numero de vuelo\n"
-               +"7. Modificar numero de asientos disponibles\n "
+               +"7. Modificar numero de asientos disponibles\n"
                +"8. Consulta de Conexiones\n"
-               +"9. Agregar Conexiones\n"
-               +"10. Eliminar Conexiones\n"
-               +"11. Salir")
+               +"9. Salir")
         
-        resp = int(input("¿Que desea hacer?\n"))
+    resp = int(input("¿Que desea hacer?\n"))
         
     if resp ==1:
         cod_vuelo=input("Porfavor ingrese el codigo de vuelo para crear una reserva\n")
@@ -36,7 +33,7 @@ def menu():
             print("\nReservacion fallida. Ya no quedan asientos disponibles en este vuelo\n")
         else:
             print("\nIngrese un vuelo existente\n")
-        menu()
+        
 
     if resp ==2:
         cod_vuelo = input("Porfavor ingrese el codigo de vuelo para eliminar la reserva\n")
@@ -46,7 +43,7 @@ def menu():
             log("Se elimino una reserva del vuelo " + cod_vuelo)
         else:
             print("\nIngrese un vuelo existente\n")
-        menu()
+        
 
     if resp ==3:
         aero= input("Ingrese nombre de la aerolinea:\n")
@@ -60,7 +57,7 @@ def menu():
         agregar_vuelo(aero,origen,destino,codigo, salida,durar,llegada,asientos)
         print("Vuelo Guardado exitosamente")
         log("Se registro un nuevo vuelo, con código "+ codigo)
-        menu()
+        
         
     if resp ==4:
         cod = input("Ingrese el codigo del vuelo que desea eliminar:\n")
@@ -70,10 +67,10 @@ def menu():
             log("Se elimino "+ cod)
         else:
             print("\nIngrese un vuelo existente\n")
-        menu()
+        
         
     if resp ==5:
-        numeroV = input("Ingrese el codigo de vuelo a modificar")
+        numeroV = input("Ingrese el codigo de vuelo a modificar\n")
         salida= input("Ingrese la nueva hora de salida en formato militar (e.g.: 16:30):\n")
         comp = mod_hora(numeroV,salida)
         if comp == True:
@@ -81,10 +78,10 @@ def menu():
             log("Se cambió la hora de salida del vuelo "+ numeroV + "a las " + salida)
         else:
             print("\nIngrese un vuelo existente o una hora valida\n")
-        menu()
+        
 
     if resp ==6:
-        numeroV = input("Ingrese el codigo de vuelo a modificar")
+        numeroV = input("Ingrese el codigo de vuelo a modificar\n")
         numeroN = input("Ingrese el nuevo numero de vuelo:\n")
         comp = mod_num_vuelo(numeroV,numeroN)
         if comp == True:
@@ -92,45 +89,36 @@ def menu():
             log("Se cambio el vuelo "+numeroV + "al nuevo codigo "+numeroN)
         else:
             print("\nIngrese un vuelo existente\n")
-        menu()
+        
         
     if resp ==7:
         numeroV= input("Ingresar el codigo de vuelo:\n")
-        numeroA= input("Cuantos asientos nuevos?:\n")
-        mod_asientos_disp(numeroV,numeroA)
-        print("Numero de asientos cambiados exitosamente")
-        log("\nModificacion asientos\n")
-        menu()
-#AQUI COMIENZA LO FALSO
-    #-------------------------------------------------------------------------------------
-    #YOU ARE ABOUT TO WITNESS THE STRENGTH OF STREET KNOWLEDGE
+        numeroA= input("Ingresar el nuevo numero de asientos disponibles:\n")
+        comp = mod_asientos_disp(numeroV,numeroA)
+        if comp == True:
+            print("Numero de asientos cambiados exitosamente")
+            log("Se cambio el numero de asientos disponibles del vuelo " + numeroV + "a " + numeroA)
+        
+
     if resp ==8:
         ciudad1 = input("Ingrese la primer ciudad para buscar conexion:\n")
         ciudad2 = input("Ingrese la segunda ciudad a conectar:\n")
-        conexion = disp_conexiones(ciudad1,ciudad2)
-        log("Descubrir Conexiones")
-        print("Estas son las conexiones disponibles"+conexion)
-        menu()
+        conexiones, tiempo = consulta_conexion(ciudad1,ciudad2)
+        if len(conexiones) == 2:
+            print("La ruta recomendada es tomando los vuelos " + conexiones[0] + " y "+ conexiones[1] +", en un tiempo de " + tiempo + " horas")
+            log("Se consulto una conexion entre " + ciudad1 + " y " +ciudad2)
+        elif len(conexiones) == 1:
+            print("La ruta recomendada es tomando el vuelo " + conexiones[0] +", en un tiempo de " + tiempo + " horas")
+            log("Se consulto una conexion entre " + ciudad1 + " y " + ciudad2)
+        else:
+            print("No existe una conexion entre esas dos ciudades o esas ciudades no se encuentran en la base de datos")
+        
+        
     if resp ==9:
-        ciudad1= input("Ingrese la primera ciudad para una nueva conexion:\n")
-        ciudad2= input("Ingrese la segunda ciudad para una nueva conexion:\n")
-        conexionN = nueva_conexion(ciudad1, ciudad2)
-        log("\nNueva Conexion")
-        print("Nueva Conexion Guardada con exito")
-        menu()
-    if resp ==10:
-        ciudad1 = input("Ingrese la ciudad1 para eliminar su conexion:\n")
-        ciudad2= input("Ingrese la ciudad2 para eliminar su conexion:\n")
-        eliminar_conexion(ciudad1,ciudad2)
-        log("\nEliminar Conexion")
-        print("La conexion fue eliminada")
-    if resp ==11:
         salir()
-    else:
-        print("Porfavor Ingrese un numero correcto")
-        log("\nTe COnFunDIsTe wEEEE xDxDxD")
-        menu()
-def salir():
-    print("Gracias por utilizar este servicio")
-    actualizar_csv()
-menu()
+        break
+        
+
+        
+
+
